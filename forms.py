36c9defd08c7302d214e922d58b82cbd7982a77b
Anjani Bhargav kumar kalepu <142475730+bhargavkumar2304@users.noreply.github.com>
2025-10-28 16:06:46 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, EqualTo
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired()])
-    password = PasswordField('Password', validators=[InputRequired()])
-    submit = SubmitField('Login')
+from wtforms.validators import DataRequired, EqualTo, Length
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=3, max=100)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=4)])
-    confirm = PasswordField('Confirm Password', validators=[EqualTo('password')])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
 class TaskForm(FlaskForm):
-    title = StringField('Task', validators=[InputRequired()])
+    title = StringField('Task Title', validators=[DataRequired()])
     submit = SubmitField('Save')
